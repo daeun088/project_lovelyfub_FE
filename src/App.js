c8a76a847258e2843bff6eff392825b347eab468
daeun088 <kde2329@naver.com>
2023-08-14 12:25:28 +0900

    Hash -> Browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import './App.css';
 import { LoginProvider } from './components/Layout/Header/Login/LoginContext';
 import Header from './components/Layout/Header/Header';
