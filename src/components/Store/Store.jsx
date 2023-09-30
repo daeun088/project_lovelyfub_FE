e4bd1238fdf3b2a73d782949501a61bb2bffcb32
daeun088 <kde2329@naver.com>
2023-09-30 22:19:56 +0900

    store 필터, 카테고리 바 추가

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -79,7 +79,24 @@ function Store() {
 
   return (
     <div className={styles.layout}>
+      <div className={styles.filterBar}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <path d="M11.2188 13.1667C11.5553 13.1667 11.8281 13.4325 11.8281 13.7604C11.8281 14.0883 11.5553 14.3542 11.2188 14.3542H8.78125C8.44471 14.3542 8.17188 14.0883 8.17188 13.7604C8.17188 13.4325 8.44471 13.1667 8.78125 13.1667H11.2188ZM13.6562 9.20833C13.9928 9.20833 14.2656 9.47418 14.2656 9.80208C14.2656 10.13 13.9928 10.3958 13.6562 10.3958H6.34375C6.0072 10.3958 5.73438 10.13 5.73438 9.80208C5.73438 9.47418 6.0072 9.20833 6.34375 9.20833H13.6562ZM16.0938 5.25C16.4303 5.25 16.7031 5.51583 16.7031 5.84375C16.7031 6.17167 16.4303 6.4375 16.0938 6.4375H3.90625C3.5697 6.4375 3.29688 6.17167 3.29688 5.84375C3.29688 5.51583 3.5697 5.25 3.90625 5.25H16.0938Z" fill="#313131"/>
+        </svg>
+        필터설정
+      </div>
+
       <div className={styles.title}>푸드리퍼브 재료로<br />음식을 만드는 식당이에요</div>
+      
+      <div className={styles.categoryBar}>
+        <ul>
+          <li>전체보기</li>
+          <li>채식</li>
+          <li>친환경</li>
+          <li>푸드리퍼브</li>
+        </ul>
+      </div>
+
       <div className={styles.cafeContainer}>
       {data.map((store) => (
         <div key={store.id} className={styles.cafeList} onClick={() => handleItemClick(store.id)}>
