ee1b101e037c9cfb9625514c0802abf9617d4a13
daeun088 <kde2329@naver.com>
2023-11-19 02:45:25 +0900

    setting input 수정

--- src/components/User/ProfileSetting.jsx
+++ src/components/User/ProfileSetting.jsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, {useState}from "react";
 import styles from './Setting.module.scss';
+import { useNavigate } from "react-router-dom";
 
 function ProfileSetting(){
+    const navigate = useNavigate();
+
+    const [image, setImage] = useState(null);
+    const [nickname, setNickname] = useState("");
+    const [snsId, setSnsId] = useState("");
+
+    const handleImageChange = (event) => {
+        const selectedImage = event.target.files[0];
+
+        // 이미지를 상태에 업데이트
+        setImage(URL.createObjectURL(selectedImage));
+    };
+
+    function handleClick() {
+        navigate('/user/mypage');
+    }
+
     return (
         <div className={styles.layout}>
             <div className={styles.pictureContainer}>
-                <img className={styles.picture}/>
-                <div className={styles.picSetting}>사진 수정</div>
+            <img className={styles.picture} src={image} alt="Profile" />
+                <div className={styles.picSetting}>
+                    {/* input 태그를 통해 파일 업로드 기능 추가 */}
+                    <label htmlFor="imageUpload">사진 수정</label>
+                    <input
+                        type="file"
+                        style={{display :"none"}}
+                        id="imageUpload"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                    />
+                </div>
             </div>
 
             <div className={styles.userInfo}>
                 <div className = {styles.infoBlock}>
                     <div className={styles.nickname}>닉네임</div>
-                    <input className={styles.inputBlock} placeholder="닉네임"/>
+                    <input className={styles.inputBlock} placeholder="닉네임"
+                    value={nickname}
+                    onChange={(e) => setNickname(e.target.value)}
+                    />
                 </div>
                 <div className = {styles.infoBlock}>
                     <div className={styles.nickname}>SNS ID</div>
-                    <input className={styles.inputBlock} placeholder="SNS ID"/>
+                    <input className={styles.inputBlock} placeholder="SNS ID"
+                    value={snsId}
+                    onChange={(e) => setSnsId(e.target.value)}/>
                 </div>
             </div>
-            <div className={styles.button}>수정완료</div>
+            <div className={styles.button} onClick={handleClick}>수정완료</div>
         </div>
     )
 }
